6a1c2b4c242a6a501fff14cc444646d40661861d
mrarslonbe22-create <mrarslonbe22@gmail.com>
2026-05-01 17:11:24 +0500

    Add TestState for handling test-taking process

--- states.py
+++ states.py
@@ -4,6 +4,9 @@ class RegisterState(StatesGroup):
     waiting_for_first_name = State()
     waiting_for_last_name = State()
 
+class TestState(StatesGroup):
+    taking_test = State()
+
 class AddTestState(StatesGroup):
     waiting_for_collection_name = State()
     waiting_for_question = State()
