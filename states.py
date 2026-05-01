9902e59c9262f8472e52cc3f65e07213cec8b8c6
mrarslonbe22-create <mrarslonbe22@gmail.com>
2026-05-01 16:35:22 +0500

    Add state management for registration and tests

---
+++ states.py
@@ -0,0 +1,12 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class RegisterState(StatesGroup):
+    waiting_for_first_name = State()
+    waiting_for_last_name = State()
+
+class AddTestState(StatesGroup):
+    waiting_for_collection_name = State()
+    waiting_for_question = State()
+    waiting_for_options = State()
+    waiting_for_correct_option = State()
+    waiting_for_more = State()
